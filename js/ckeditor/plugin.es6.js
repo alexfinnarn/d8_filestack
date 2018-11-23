7c463f0e2826addd321348a04264a0ebf0eee150
Alex Finnarn <alexfinnarn@gmail.com>
2018-11-23 17:39:37 -0500

    initial module

---
+++ js/ckeditor/plugin.es6.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ *
+ * CKEditor plugin for Filestack picker.
+ */
+(function($, Drupal, CKEDITOR) {
+
+  CKEDITOR.plugins.add('filestack', {
+    // requires: 'image2',
+    // icons: 'drupalimage',
+    hidpi: true,
+
+  });
+
+})(jQuery, Drupal, CKEDITOR);
